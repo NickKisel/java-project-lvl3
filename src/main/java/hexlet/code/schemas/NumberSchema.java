eab7c8f67e46b8ef4633391aaeb18d0290776a4f
Nick <insherlolqq@gmail.com>
2022-01-22 09:45:38 +0300

    rework NumberSchema method positive()

--- src/main/java/hexlet/code/schemas/NumberSchema.java
+++ src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -16,8 +16,8 @@ public final class NumberSchema extends BaseSchema {
     }
 
     public NumberSchema positive() {
-        super.forValidation.clear();
-        Predicate<Object> isPositive = object -> object == null || (Integer) object >= 0;
+        required();
+        Predicate<Object> isPositive = object -> (Integer) object >= 0;
         super.forValidation.add(isPositive);
         return this;
     }
