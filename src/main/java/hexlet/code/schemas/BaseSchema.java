626621d861b16565580176a8adbda7a0e3efe468
Nick <insherlolqq@gmail.com>
2022-01-26 21:23:45 +0300

    made predicates private, + protected method getPredicates

--- src/main/java/hexlet/code/schemas/BaseSchema.java
+++ src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class BaseSchema {
-    protected List<Predicate<Object>> predicates = new LinkedList<>();
+    private List<Predicate<Object>> predicates = new LinkedList<>();
 
-    /**
-     * @param object is the target for validation
-     * @return is boolean
-     */
-    public boolean isValid(Object object) {
+    protected final List<Predicate<Object>> getPredicates() {
+        return predicates;
+    }
+
+    public final boolean isValid(Object object) {
         return predicates.stream()
                 .allMatch(x -> x.test(object));
     }
